25829d491458ee085244951e296c26c6b9975811
tad1693 <tadmhi@gmail.com>
2019-07-09 09:32:49 -0600

    More unit test to dateHandler.js

--- tests/unit/dateHandler.spec.js
+++ tests/unit/dateHandler.spec.js
@@ -2,11 +2,11 @@ import DateHandler from '@/util/dateHandler'
 
 const sprintWeek = 'weekly 0513'
 const dateHandler = new DateHandler()
-const dateOutOfETA = '2019-05-21T14:41:04Z'
+const dateOutOfETA = '2019-05-18T14:41:04Z'
 const dateInETA = '2019-05-16T14:41:04Z'
 const dateBeforeETA = '2019-05-10T14:41:04Z'
 const dateWeekTag = '2019-05-13T06:00:00.000Z'
-
+const lastDateOfSprint = '2019-05-18T06:00:00.000Z'
 dateHandler.weeklyTag = 'weekly 0513'
 
 describe('Date Handler', () => {
@@ -16,13 +16,18 @@ describe('Date Handler', () => {
   })
   test('should return last date of sprint', () => {
     expect(dateHandler.getLastDayOfSprint().toDate())
-      .toEqual(new Date(dateOutOfETA))
+      .toEqual(new Date(lastDateOfSprint))
   })
-  test('Should be out of ETA', () => {
-    expect(dateHandler.isOutOfETA(sprintWeek, dateOutOfETA)).toBeTruthy()
+  test('should return 3 days of Sprint', () => {
+    expect(dateHandler.daysOfSprint(sprintWeek, dateInETA)).toBe(3)
   })
-  test('Should be on ETA', () => {
-    expect(dateHandler.isOutOfETA(sprintWeek, dateInETA)).toBeFalsy()
+  describe('is Out of ETA', () => {
+    test('Should be out of ETA', () => {
+      expect(dateHandler.isOutOfETA(sprintWeek, dateOutOfETA)).toBeTruthy()
+    })
+    test('Should be on ETA', () => {
+      expect(dateHandler.isOutOfETA(sprintWeek, dateInETA)).toBeFalsy()
+    })
   })
   describe('Within Sprint function', () => {
     test('Date before sprint should be out of ETA', () => {
